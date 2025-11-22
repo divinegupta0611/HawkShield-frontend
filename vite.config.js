3116e637507946f30433b15b427840d31364aa6c
divine <guptadivine0611@gmail.com>
2025-11-22 17:00:49 +0530

    modified google auth

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",  
   plugins: [react()],
+  envDir: './', // Explicitly tell Vite where to look for .env
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+    },
+  },
 })
